9d164550455028e27ff2715379dfabde30200100
Adrian Moreno <amorenoz@redhat.com>
2025-07-18 14:53:13 +0200

    Add ContainerFile and deployment
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+usage()
+{
+    /bin/echo -e "This is an entrypoint script for VDUSE Device Plugin"
+    /bin/echo -e ""
+    /bin/echo -e "./entrypoint.sh"
+}
+
+exec vduse-dp
